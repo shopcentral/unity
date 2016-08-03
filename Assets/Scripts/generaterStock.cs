315300f4ce4142f2bcf2539ff3db3f31a486eae4
swacky <student@Just-Chilling.local>
2016-08-03 16:58:27 +1000

    1st real commit

---
+++ Assets/Scripts/generaterStock.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+//[System.Serializable]
+//public struct xyz
+//{
+// public float x,y,z;
+//};
+[System.Serializable]
+public struct Item
+{
+	public string Name,shape;
+	public int mode;
+	public Vector3 pos;
+	public Quaternion rot;
+	public int scale;
+};
+[System.Serializable]
+public class ItemHolder
+{
+	public Item[] items;
+}
+public class generaterStock : MonoBehaviour {
+
+	private string db = "";
+
+	void LoadStock(){ // my own function
+		
+		GameObject item, addItem; //create item varible
+		Vector3 pos, lastspot, diff;
+		pos = lastspot = diff = new Vector3 (0, 0, 0);
+
+		//import db
+		string fileName = "Assets/Scripts/sampletest.json";
+		try{
+			StreamReader theFile = new StreamReader(fileName);
+			using (theFile) {
+				db = theFile.ReadToEnd ();
+			}
+		}
+		catch {
+			db = "";
+			print ("catch");
+		}
+
+		//change the db to json so unity can read
+		ItemHolder stuff = JsonUtility.FromJson<ItemHolder> (db);
+
+		// instantiate the items into the gameplay
+		foreach (Item stock in stuff.items) {
+
+			Quaternion rot = stock.rot;
+			rot.w = 1;
+			Vector3 scale = new Vector3 (1, 1, 1) * (stock.scale + 1);
+			addItem = Resources.Load (stock.shape) as GameObject;
+			if (addItem) {
+
+				if (stock.mode == 0) {
+					pos = stock.pos;
+					diff = lastspot - pos;
+					lastspot = pos;
+				}
+				if (stock.mode == 1) {
+					pos = pos - diff;
+				}
+				item = Instantiate (addItem, pos, rot) as GameObject; // place the stock in the game
+				item.name =stock.Name;
+				item.transform.localScale = scale;
+				item.transform.parent = transform;
+
+			} else
+				print ("can't instantiate");
+
+		}
+		//Vector3 scale = addItem.transform.localScale;
+
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+		LoadStock (); // function call
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	public GameObject head;
+	
+	public float speed = 10f;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButton (0)) {
+			Debug.Log ("Pressed left click.");
+			GetComponent<Rigidbody> ().AddForce (1 * speed * head.transform.forward);
+		}
+		Transform tr;
+		//		if (Input.GetAxis("Vertical") > 0)
+		//		{
+		//			transform.Translate(0f,0f,0.1f);
+		//		}
+		GetComponent<Rigidbody>().AddForce (Input.GetAxis("Vertical") * speed * transform.forward);
+
+		tr= GetComponent<Transform> ();
+		tr.eulerAngles = new Vector3(0,tr.eulerAngles.y,0);
+
+		transform.Rotate (0,Input.GetAxis("Horizontal"),0);
+	}
+}
